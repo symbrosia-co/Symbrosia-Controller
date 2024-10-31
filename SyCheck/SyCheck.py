feed23bf5fa24e34a8b795fe7d23bbf26694c87c
DarkerView <acooper@pobox.com>
2024-10-31 09:01:37 -1000

    SyCheck v0.2

--- SyCheck/SyCheck.py
+++ SyCheck/SyCheck.py
@@ -5,13 +5,15 @@
 #  - written for Python v3.9
 #
 #  Symbrosia
-#  Copyright 2021, all rights reserved
+#  Copyright 2024, all rights reserved
 #
 # 23Oct2024 A. Cooper v0.1
 # - initial version
+# 23Oct2024 A. Cooper v0.2
+# - test version released to cultivation
 #
 #------------------------------------------------------------------------------
-verStr= 'SyCheck v0.1'
+verStr= 'SyCheck v0.2'
 
 #-- constants -----------------------------------------------------------------
 cfgFileName= 'configuration.xml'
@@ -92,7 +94,7 @@ class Application(tk.Frame):
     self.scanButton.grid   (column=1,row=2,padx=spaceX,pady=spaceY)
     self.scanAllButton=    tk.Button(self,text="Scan All",width=10,command=self.scanAll,font=("Helvetica", "12"))
     self.scanAllButton.grid(column=1,row=3,padx=spaceX,pady=spaceY)
-    self.saveButton=       tk.Button(self,text="Save",width=10,command=self.save,font=("Helvetica", "12"))
+    self.saveButton=       tk.Button(self,text="Save",width=10,command=self.saveReport,font=("Helvetica", "12"))
     self.saveButton.grid   (column=1,row=4,padx=spaceX,pady=spaceY)
     self.quitButton=       tk.Button(self,text="Quit",width=10,command=self.done,font=("Helvetica", "12"))
     self.quitButton.grid   (column=1,row=5,padx=spaceX,pady=spaceY)
@@ -162,21 +164,26 @@ class Application(tk.Frame):
 
   def scan(self):
     self.report= []
+    self.report.append('SyCheck Report')
+    self.report.append('  {:%Y-%m-%d %H:%M:%S}'.format(dt.datetime.now()))
+    self.report.append('  ')
     for unit in self.units:
       if unit['name']==self.ctrlStr.get():
         self.scanUnit(unit)
+    self.logEvent('  Scan {} comlete'.format(unit['name']),True)
       
   def scanAll(self):
     self.report= []
     self.report.append('SyCheck Report')
     self.report.append('  {:%Y-%m-%d %H:%M:%S}'.format(dt.datetime.now()))
-    self.report.append('  ')
+    self.report.append('')
     for unit in self.units:
       self.scanUnit(unit)
-    for line in self.report:
-      print(line)
+    self.logEvent('  Scan all comlete',True)
 
   def scanUnit(self,unit):
+      header= '  Register               Controller   Reference        Description'
+      headline= '  -------------------------------------------------------------------------------------------'
       self.logEvent('Check config for {} against {}...'.format(unit['name'],unit['ref']),True)
       self.report.append('Check config for {} against {}...'.format(unit['name'],unit['ref']))
       diffs= 0
@@ -187,45 +194,80 @@ class Application(tk.Frame):
           self.report.append('  Communications error with {}'.format(unit['name']))
         else:
           self.logEvent('  {} configuration read'.format(unit['name']),True)
-          self.report.append('  {} configuration read'.format(unit['name']))
+          self.report.append('  {} configuration read from {}'.format(unit['name'],unit['address']))
+          firstErr= True
           for reg in self.refs[unit['ref']]:
             regValue= self.controller.value(reg['reg'])
             refValue= self.controller.convert(reg['reg'],reg['value'])
             if reg['type']=='str':
               if regValue!=refValue:
+                if firstErr:
+                  self.report.append(header)
+                  self.report.append(headline)
+                  firstErr= False
                 self.logEvent('  {}:\'{}\' ≠ \'{}\' in reference'.format(reg['reg'],regValue,refValue),True)
-                self.report.append('  {}:\'{}\' ≠ \'{}\' in reference'.format(reg['reg'],regValue,refValue))
+                self.report.append('  {:<16} {:>16} ≠ {:<16} {}'.format(reg['reg'],regValue,refValue,self.controller.description(reg['reg'])))
                 diffs+=1
             if reg['type']=='int' or reg['reg']=='uint':
               if regValue!=refValue:
+                if firstErr:
+                  self.report.append(header)
+                  self.report.append(headline)
+                  firstErr= False
                 self.logEvent('  {}:{:d} ≠ {:d} in reference'.format(reg['reg'],regValue,refValue),True)
-                self.report.append('  {}:\'{:d}\' ≠ \'{:d}\' in reference'.format(reg['reg'],regValue,refValue))
+                self.report.append('  {:<16} {:>16d} ≠ {:<16d} {}'.format(reg['reg'],regValue,refValue,self.controller.description(reg['reg'])))
                 diffs+=1
             if reg['type']=='float':
               if round(regValue,4)!=round(refValue,4):
+                if firstErr:
+                  self.report.append(header)
+                  self.report.append(headline)
+                  firstErr= False
                 self.logEvent('  {}:{:.2f} ≠ {:.2f} in reference'.format(reg['reg'],regValue,refValue),True)
-                self.report.append('  {}:\'{:.2f}\' ≠ \'{:.2f}\' in reference'.format(reg['reg'],regValue,refValue))
+                self.report.append('  {:<16} {:>16.2f} ≠ {:<16.2f} {}'.format(reg['reg'],regValue,refValue,self.controller.description(reg['reg'])))
                 diffs+=1
             if reg['type']=='bool':
               if regValue!=refValue:
+                if firstErr:
+                  self.report.append(header)
+                  self.report.append(headline)
+                  firstErr= False
                 self.logEvent('  {}:{} ≠ {} in reference'.format(reg['reg'],regValue,refValue),True)
-                self.report.append('  {}:\'{}\' ≠ \'{}\' in reference'.format(reg['reg'],regValue,refValue))
+                if regValue: regValue='On'
+                else: regValue= 'Off'
+                if refValue: refValue='On'
+                else: refValue= 'Off'
+                self.report.append('  {:<16} {:>16} ≠ {:<16} {}'.format(reg['reg'],regValue,refValue,self.controller.description(reg['reg'])))
                 diffs+=1
           if diffs==0:
             self.logEvent('  No differences found'.format(diffs),True)
             self.report.append('  No differences found'.format(diffs))
-          if diffs==1:
+          elif diffs==1:
             self.logEvent('  {:d} difference found'.format(diffs),True)
             self.report.append('  {:d} difference found'.format(diffs))
           else:
             self.logEvent('  {:d} differences found'.format(diffs),True)
             self.report.append('  {:d} differences found'.format(diffs))
+          self.report.append('')
       else:
         self.logEvent('  Error!! Unable to open {}'.format(unit['name']),True)
-        self.report.append('  Error!! Unable to open {}'.format(unit['name']))        
-    
-  def save(self):
-    pass
+        self.report.append('  Error!! Unable to open {}'.format(unit['name']))    
+
+  def saveReport(self):
+      type= [('Text', '*.txt')]
+      name= 'SyCheck{:%Y%m%d}'.format(dt.datetime.now())
+      file= filedialog.asksaveasfilename(title='Save report...',initialfile=name,filetypes=type,defaultextension=type,initialdir=localDir)
+      if file==None: return
+      try:
+        reportFile= open(file,'w',encoding="utf-8")
+      except:
+        messagebox.showwarning(title='File error...', message='Unable to open file {}'.format(file))
+        return
+      for line in self.report:
+        reportFile.write(line)
+        reportFile.write('\n')
+      reportFile.close()
+      self.logEvent('Report {} saved'.format(os.path.basename(file)),True)  
 
   # handle the quit button
   def done(self):
